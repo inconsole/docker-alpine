14a5a2ae7501317a738fd078413a4356c12ebb0f
inconsole <inconsole@yahoo.com.cn>
2016-10-15 01:04:29 +0800

    Add files via upload

---
+++ www/2.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "2-php";
+
+echo time();
+
+
+?>
---
+++ www/hello.php
@@ -0,0 +1,13 @@
+<?php
+
+$con = mysql_connect("db","root","");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+echo 'links';
+
+
+
+?>
